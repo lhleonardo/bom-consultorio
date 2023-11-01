f7024b053d6115482148328c57cae7b724e041ff
Leonardo Braz <lhleonardo05@gmail.com>
2023-10-31 23:43:49 -0300

    adiciona colunas para a tabela empresa

--- src/main/resources/db/migration/V0__criacao_base_inicial.sql
+++ src/main/resources/db/migration/V0__criacao_base_inicial.sql
@@ -1,8 +1,11 @@
 CREATE table empresa
 (
-    id            SERIAL PRIMARY KEY,
-    identificador UUID         NOT NULL,
-    razao_social  VARCHAR(255) NOT NULL,
-    nome_fantasia VARCHAR(255) not null,
-
+    id             SERIAL PRIMARY KEY,
+    identificador  UUID                     NOT NULL,
+    razao_social   VARCHAR(255)             NOT NULL,
+    nome_fantasia  VARCHAR(255)             not null,
+    site           varchar(255) null,
+    logotipo       varchar(255) null,
+    data_criacao   timestamp with time zone not null default now(),
+    data_alteracao timestamp with time zone not null default now()
 );
